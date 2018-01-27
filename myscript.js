df8524bcc05eadfb5c1d4b502e72b474598fefa9
Tiffany Xiao <txiao8415@bths.edu>
2018-01-27 18:53:22 -0500

    Added Javascript.

---
+++ myscript.js
@@ -0,0 +1,5 @@
+window.addEventListener("mousedown", changeImage());
+
+function changeImage(){
+	document.getElementsByClassName("_2poz _ui9")[0].setAttribute("style", "background-image: url("Images/mebun3.png"); background-repeat: no-repeat; background-size: 120px 120px; cursor: pointer; height: 120px; width: 120px;)");
+}
