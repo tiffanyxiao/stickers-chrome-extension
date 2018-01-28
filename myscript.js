613fd97498abc6e25765ff798b40b7f46f8ae7ef
Aaron <wang.aa@husky.neu.edu>
2018-01-27 19:32:00 -0500

    bun

--- myscript.js
+++ myscript.js
@@ -1,5 +1,20 @@
-window.addEventListener("mousedown", changeImage());
+
 
 function changeImage(){
-	document.getElementsByClassName("_2poz _ui9")[0].setAttribute("style", "background-image: url("Images/mebun3.png"); background-repeat: no-repeat; background-size: 120px 120px; cursor: pointer; height: 120px; width: 120px;)");
+  var list = document.getElementsByClassName("_2poz _ui9");
+  console.log("changeImage()");
+
+  for (i = 0; i < list.length; i++) {
+    //if (list[i].style["background-image"] == "background-image: url(&quot;/stickers/asset/?sticker_id=551710701530728&amp;image_type=BestEffortImage&quot;); background-repeat: no-repeat; background-size: 120px 120px; cursor: pointer; height: 120px; width: 120px;") {
+      list[i].setAttribute("style", "background-image: url(\"https://i.imgur.com/7Ix7cfw.png\"); background-repeat: no-repeat; background-size: 120px 120px; cursor: pointer; height: 120px; width: 120px;)");
+    //}
+  }
 }
+
+
+document.addEventListener("onclick", changeImage());
+console.log("myscript loaded")
+
+//https://i.imgur.com/3JMolm4.png
+//https://i.imgur.com/v2OkVQd.png
+//https://i.imgur.com/7Ix7cfw.png
